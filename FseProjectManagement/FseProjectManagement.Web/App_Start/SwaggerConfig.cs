147847764ceb5940b00f823fdad7da842b33cad2
Kishor Bhosale <kishor.bhosale@cognizant.com>
2019-09-01 20:27:33 +0800

    FSE Project Management: Swagger changes

---
+++ FseProjectManagement/FseProjectManagement.Web/App_Start/SwaggerConfig.cs
@@ -0,0 +1,26 @@
+using FseProjectManagement.Web;
+using Swashbuckle.Application;
+using System.Web.Http;
+using WebActivatorEx;
+
+[assembly: PreApplicationStartMethod(typeof(SwaggerConfig), "Register")]
+
+namespace FseProjectManagement.Web
+{
+    public class SwaggerConfig
+    {
+        public static void Register()
+        {
+            var thisAssembly = typeof(SwaggerConfig).Assembly;
+
+            GlobalConfiguration.Configuration
+                .EnableSwagger(c =>
+                    {
+                        c.SingleApiVersion("v1", "Project Manager");
+                    })
+                .EnableSwaggerUi(c =>
+                    {
+                    });
+        }
+    }
+}
